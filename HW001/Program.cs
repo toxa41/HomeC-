8be2754512a05a24e02ce0206a4557b63857fc35
Anton Orlov <toxa41@list.ru>
2022-10-30 23:55:13 +0300

    Task done

--- HW001/Program.cs
+++ HW001/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("12Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Максимальное число: " + numberA);
+    Console.WriteLine("Минимальное число: " + numberB);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + numberB);
+    Console.WriteLine("Минимальное число: " + numberA);
+}
