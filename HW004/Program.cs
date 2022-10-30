856f60e26d49b30296adaac34096d5a1e64c9c29
Anton Orlov <toxa41@list.ru>
2022-10-31 00:30:31 +0300

    Task 4 done

---
+++ HW004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 0;
+while (numberB < numberA)
+{
+    numberB++;
+    if (numberB % 2 == 0)
+    {
+        Console.Write(numberB + " ");
+
+    }
+    
+}
