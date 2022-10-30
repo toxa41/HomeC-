3d35eb32a4f55d7b7f9d0e591e4b79ea38c36472
Anton Orlov <toxa41@list.ru>
2022-10-31 00:12:50 +0300

    Task 3 done

---
+++ HW003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное!");
+}
+else
+{
+    Console.WriteLine("Число не четное!");
+}
