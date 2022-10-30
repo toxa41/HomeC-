e6ae42e4499b244ed1c223eaa78d8890c1f0441c
Anton Orlov <toxa41@list.ru>
2022-10-31 00:07:26 +0300

    Task 2 done

---
+++ HW002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (numberA > numberB)
+{
+    max = numberA;
+}
+  
+if (max < numberC)
+{
+    max = numberC;
+}
+Console.WriteLine("Максимальное из трех чисел: " + max);
+
+  
