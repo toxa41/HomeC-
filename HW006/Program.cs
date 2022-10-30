83e8df1ae8da1123622373614826c06a43be01d4
Anton Orlov <toxa41@list.ru>
2022-10-31 02:59:06 +0300

    Create task6 + diargam

---
+++ HW006/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (number >= 100)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    result = number % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Третьей циры нет");
+}
+
+
