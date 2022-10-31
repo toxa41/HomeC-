285786670bafdf9271375698ddbd11dc01f51752
Anton Orlov <toxa41@list.ru>
2022-11-01 01:18:40 +0300

    Create task 9 + diagram + readme

---
+++ HW_3/HW009/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number;
+int reverse = 0;
+int division = 0;
+while (number > 0)
+{
+    division = number % 10;
+    reverse = reverse * 10 + division;
+    number = number / 10;
+}
+if (temp == reverse)
+{
+    Console.WriteLine("Это палиндром!");
+}
+else
+{
+    Console.WriteLine("Это не палиндром!");
+}
