c34cbade49a84d660f0320cd6e2f354ec62eaeae
Anton Orlov <toxa41@list.ru>
2022-10-31 03:41:56 +0300

    Create task7 + diargam + readme

---
+++ HW007/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите цифру дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day > 7)
+{
+    Console.WriteLine("Ошибка ввода! В неделе всего 7 дней!");
+}
+else if (day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
+
+
