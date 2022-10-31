9a16645ee03344e8ab1ad76855c53b5e7525e04c
Anton Orlov <toxa41@list.ru>
2022-11-01 00:28:18 +0300

    Create task 8

---
+++ HW_3/HW008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count < number+1)
+{
+    Console.WriteLine(Math.Pow(count, 3));
+    count++;
+}
