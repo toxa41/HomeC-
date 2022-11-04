ab2b9adf4259a4f963e9f4740e244752983b1dab
Anton Orlov <toxa41@list.ru>
2022-11-05 01:29:38 +0300

    Create hw suum number

---
+++ HW_4/HW012/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, 
+// которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int summ = 0;
+
+int calculate(int num1)
+{
+    int temp = 0;
+    int summ = 0;
+    while (num1 > 0)
+    {
+        temp = num1 % 10;
+        summ = summ + temp;
+        num1 = num1 / 10;
+
+    }
+    return summ;
+}
+int allSumm = calculate(number);
+Console.WriteLine($"Сумма цифр числа равна: {allSumm}");
---
+++ HW_4/HW013/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите длинну массива: ");
+int [] array = new int [20];
+int rand = new Random();
+for (int i = 0; i < len; i++)
+
+{
+    array[i] = rand.Next(1,5);
+}
+Console.WriteLine(rand);
