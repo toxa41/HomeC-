f01ef40c49a257af36923c8f61bcee01cfb97801
Anton Orlov <toxa41@list.ru>
2022-11-07 01:23:49 +0300

    Done task 36

---
+++ HW_5/HW015/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+ArrayRandom(numbers);
+Console.Write("Массив: ");
+OutputArray(numbers);
+int summ = 0;
+
+
+for (int j = 0; j < numbers.Length; j += 2)
+
+    summ += numbers[j];
+
+
+Console.WriteLine($"Сумма нечетных позиций равна: {summ}");
+
+
+void ArrayRandom(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 10);
+    }
+}
+
+void OutputArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
