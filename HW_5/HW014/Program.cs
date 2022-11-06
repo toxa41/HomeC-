27bb3012217f47446525de7eeea81cb25c1bb6bd
Anton Orlov <toxa41@list.ru>
2022-11-06 22:46:31 +0300

    Done task 34

---
+++ HW_5/HW014/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+Console.Write("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+ArrayRandom(numbers);
+Console.Write("Массив: ");
+OutputArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"Четных чисел: {count}");
+
+void ArrayRandom(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,999);
+    }
+}
+void OutputArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+
+    Console.WriteLine();
+}
