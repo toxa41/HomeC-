32ccc27fd4551312a6003e7323cad2ec8ecfd21d
Anton Orlov <toxa41@list.ru>
2022-11-14 01:26:20 +0300

    Done all task

---
+++ HW_9/HW029/Program.cs
@@ -0,0 +1,24 @@
+﻿int numberM, numberN;
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью
+//рекурсии. Даны два неотрицательных числа m и n.
+//m = 3, n = 2 -> A(m,n) = 29
+
+IntputNumbers(out numberM, out numberN);
+
+int AckermannFunction(int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+    return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM}, {numberN}) = {AckermannFunction(numberM, numberN)}");
+
+void IntputNumbers(out int numberM, out int numberN)
+{
+    Console.Write("Введите число M: ");
+    numberM = int.Parse(Console.ReadLine());
+    Console.Write("Введите число N: ");
+    numberN = int.Parse(Console.ReadLine());
+}
---
+++ HW_9/HW_028/Program.cs
@@ -0,0 +1,30 @@
+﻿int numberM, numberN;
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+
+InputNumbers(out numberM, out numberN);
+
+void GapNumberSum(int numberM, int numberN, int sum)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
+
+void InputNumbers(out int numberM, out int numberN)
+{
+    Console.Write("Введите начальное число M: ");
+    numberM = int.Parse(Console.ReadLine());
+    Console.Write("Введите конечное число N: ");
+    numberN = int.Parse(Console.ReadLine());
+}
